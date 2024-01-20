722594607c202673bf0fbf83737fcfb3535396af
Ben Shaw <Ben Shaw@THINKPAD370>
2024-01-20 02:22:26 +0000

    final amendment

--- PacificTours/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PacificTours/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -116,7 +116,7 @@ namespace PacificTours.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect("/");
                 }
                 if (result.RequiresTwoFactor)
                 {
